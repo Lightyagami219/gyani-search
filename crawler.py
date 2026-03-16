503de78c64d4b883d6827c4ae576f098d236bce1
Lightyagami219 <itskekegainkai@gmail.com>
2026-03-16 14:34:28 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,6 +1,8 @@
 import json
 
-topics = [
+database=[]
+
+topics=[
 
 # physics
 "Newton's laws of motion",
@@ -13,18 +15,34 @@ topics = [
 "Kinetic energy",
 "Potential energy",
 "Wave motion",
+"Classical mechanics",
+"Angular momentum",
+"Force",
+"Work and energy",
+"Entropy",
+"Maxwell equations",
+"Planck constant",
+"Schrodinger equation",
+"Heisenberg uncertainty principle",
+"Quantum field theory",
 
-# chemistry
-"Atom",
-"Molecule",
-"Periodic table",
-"Hydrogen",
-"Oxygen",
-"Carbon",
-"Covalent bond",
-"Ionic bond",
-"Organic chemistry",
-"Inorganic chemistry",
+# astronomy
+"Black hole",
+"Supernova",
+"Galaxy",
+"Milky Way",
+"Dark matter",
+"Dark energy",
+"Event horizon",
+"Neutron star",
+"Pulsar",
+"Exoplanet",
+"Cosmic microwave background",
+"Big Bang",
+"Inflation theory",
+"Cosmic expansion",
+"Gravitational waves",
+"Space-time",
 
 # biology
 "Cell biology",
@@ -37,29 +55,39 @@ topics = [
 "Respiration",
 "Neuron",
 "Protein synthesis",
+"Enzyme",
+"Metabolism",
+"Immune system",
+"Stem cells",
+"Cell division",
+"Mitosis",
+"Meiosis",
 
-# astronomy
-"Black hole",
-"Supernova",
-"Galaxy",
-"Milky Way",
-"Dark matter",
-"Dark energy",
-"Event horizon",
-"Neutron star",
-"Exoplanet",
-"Cosmic microwave background",
+# chemistry
+"Atom",
+"Molecule",
+"Periodic table",
+"Hydrogen",
+"Helium",
+"Carbon",
+"Oxygen",
+"Nitrogen",
+"Covalent bond",
+"Ionic bond",
+"Organic chemistry",
+"Inorganic chemistry",
+"Chemical reaction",
+"Acid base reaction",
+"Redox reaction",
+"Catalyst",
 
 # cosmology
-"Big Bang",
-"Inflation theory",
-"Cosmic expansion",
 "Multiverse theory",
-"Space-time",
-"Gravitational waves",
+"Cosmic inflation",
+"Large scale structure of universe",
+"Dark energy density",
+"Cosmic background radiation",
 ]
-
-database=[]
 
 for topic in topics:
 
@@ -68,6 +96,24 @@ for topic in topics:
         "url": "https://en.wikipedia.org/wiki/" + topic.replace(" ","_"),
         "description": "Scientific topic about " + topic
     })
+
+# create many variations automatically
+
+extra=[]
+
+for item in database:
+
+    words=item["title"].split()
+
+    for w in words:
+
+        extra.append({
+            "title": w + " science",
+            "url": item["url"],
+            "description": item["description"]
+        })
+
+database.extend(extra)
 
 with open("gyani_index.json","w") as f:
     json.dump(database,f,indent=2)
